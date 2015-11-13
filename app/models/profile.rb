9761bbc002c15213d8b631b058f86adf108f68d3
dangnv <dang.nv@anlab.pro>
2015-11-13 17:10:48 +0700

    commit last T6 13/11/2015

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,6 +4,6 @@ class Profile < ActiveRecord::Base
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   validates :sex, inclusion: %w(male female)
-  validates :tel, inclusion: { in: 0..9 }, length: { maximum: 11 }
+  validates :tel, length: { maximum: 11 }
   validates :address, presence: true
 end
