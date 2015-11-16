e3e9e433920f3a857343aa3bf26725134066a437
nvdang <dang.nv@anlab.pro>
2015-11-16 23:31:59 +0700

    last commit T2 16/11/2015

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,9 @@ class UsersController < ApplicationController
      @user = current_user
      @users_following = @user.following
      @users_follower = @user.followers
-     @tweets = current_user.feed.order("created_at DESC")
   end
    
   def following
     @users = User.all
   end
-  
-  def followers
-  end
-  
 end
