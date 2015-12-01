a90e4cea11ad9ad85b7e2f903a43d392fbc29ee8
dangnv <dang.nv@anlab.pro>
2015-12-01 17:43:24 +0700

    Last commit Tuesday 1/12/2015

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,14 +18,42 @@
 //= require_tree .
 
 var ready = function() {
-    $('.typeahead').autocomplete({
-      source: "/tweets/suggest",
-      minLength: 1,
-      select: function(event, ui) {
-        $('.typeahead').val(ui.item.value);
-        $('.typeahead').submit();
-      }
-    });
+	function split( val ) {
+      return val.split( /@\s*/ );
+    }
+    function extractLast( term ) {
+      return split( term ).pop();
+    }
+    $(".typeahead")
+      .bind( "keydown", function( event ) {
+        if ( event.keyCode === $.ui.keyCode.TAB &&
+            $( this ).autocomplete( "instance" ).menu.active ) {
+          event.preventDefault();
+        }
+      })
+      .autocomplete({
+        source: function( request, response ) {
+          $.getJSON( "/tweets/suggest.json", {
+            term: extractLast( request.term )
+          }, response );
+        },
+        search: function() {
+          var term = extractLast( this.value );
+          if ( term.length < 1 ) {
+            return false;
+          }
+        },
+        focus: function() {
+          return false;
+        },
+        select: function( event, ui ) {
+          var terms = split( this.value );
+          terms.pop();
+          terms.push( ui.item.value );
+          this.value = terms.join("@");
+          return false;
+        }
+      });
 }
 
 $(document).ready(ready);
--- app/models/user.rb
+++ app/models/user.rb
@@ -71,4 +71,25 @@ class User < ActiveRecord::Base
   def self.search(term)
     where('username like ?', "%#{term}%")
   end
+  
+  def self.find_for_twitter_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    if user
+      return user
+    else
+      registered_user = User.where(:email => auth.uid + "@twitter.com").first
+      if registered_user
+        return registered_user
+      else
+
+        user = User.create(username:auth.extra.raw_info.username,
+                            provider:auth.provider,
+                            uid:auth.uid,
+                            email:auth.uid+"@twitter.com",
+                            password:Devise.friendly_token[0,20],
+                          )
+      end
+
+    end
+  end
 end
